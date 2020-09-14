f33639748e1675b8a0b1a69925be49741cdbbc04
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-09-14 23:15:50 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
